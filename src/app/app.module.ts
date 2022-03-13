5d4cf430cf62b41928abd0b156f380ab8c276147
ugrasenanv <ugrasenanv@gmail.com>
2022-03-13 23:22:45 +0530

    Code refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 import { ViewTaskComponent } from './components/view-task/view-task.component';
 import { NgxAsideModule } from 'ngx-aside';
 import { GetAllTasksService } from './task-services/get-all-task.service';
 import { GetATaskService } from './task-services/get-atask.service';
 import { SharedTaskService } from './task-services/shared.service';
-import { CreateTaskComponent } from './components/create-task/create-task.component';
 import { UpdateTaskComponent } from './components/update-task/update-task.component';
 import { ModalComponent } from './components/modal/modal.component';
 import { SaveTaskService } from './task-services/save-task.service';
@@ -25,11 +23,9 @@ import { SaveTaskService } from './task-services/save-task.service';
   ],
   declarations: [
     AppComponent,
-    HomeComponent,
     NavigationBarComponent,
     UpdateTaskComponent,
     ViewTaskComponent,
-    CreateTaskComponent,
     ModalComponent,
   ],
   bootstrap: [AppComponent],
